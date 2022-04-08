e681fa396c619d21feca8f94f6ed3bbe52dcd416
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-04-08 13:17:05 +0530

    Initial Commit

---
+++ CrudIosApp/CrudIosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrudIosAppApp.swift
+//  CrudIosApp
+//
+//  Created by Bhumika Patel on 08/04/22.
+//
+
+import SwiftUI
+
+@main
+struct CrudIosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
